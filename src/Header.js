79abb84eac0622d03f39ee6bb1dcbe46fdc091f8
Mumtahina <hina.yeasmin19@gmail.com>
2025-05-01 20:25:55 +0100

    trying to set up css

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+function Header(){
+    return (
+        <div class="Header">
+            <div>
+                <a class="logo">PIC-ME</a>
+            </div>
+            <div class="navigation-buttons">
+                <a>FAQ</a>
+                <a>PRIVACY POLICY</a>
+                <a>CONTACT ME</a>
+                <a>DONATE</a>
+            </div>
+        </div>
+    )
+}
+export default Header;
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Home(){
+    return (
+    <div class="Home">
+        <button>CLICK HERE to START</button>
+    </div>
+    )
+}
+export default Home;
