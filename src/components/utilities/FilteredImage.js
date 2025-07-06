b6de959e41949ed68494b3fe7270b433aaaeb2ba
Yeahhina <138409780+yeahhina@users.noreply.github.com>
2025-07-07 00:43:44 +0100

    trying konva

--- src/components/utilities/FilteredImage.js
+++ src/components/utilities/FilteredImage.js
@@ -1,45 +1,5 @@
-import Jimp from "jimp";
-import { useEffect, useState } from "react";
 import "./FilteredImage.css";
 
 export function FilteredImage({ imageUrl, filter }) {
-  const [jimpImage, setJimpImage] = useState(null);
-  const [image, setImage] = useState(null);
-  const [transformedImage, setTransformedImage] = useState(null);
-  useEffect(() => {
-    const loadImage = async () => {
-      const jimpImage = await Jimp.read(imageUrl);
-      setJimpImage(jimpImage);
-      const image = await jimpImage.getBase64Async(Jimp.MIME_PNG);
-      setImage(image);
-    };
-    loadImage();
-  }, [imageUrl, filter]);
-  useEffect(() => {
-    if (jimpImage) {
-      const transformImage = async () => {
-        switch (filter) {
-          case "greyscale":
-            await jimpImage.greyscale();
-            break;
-          case "sepia":
-            await jimpImage.sepia();
-            break;
-          case "natural":
-            await jimpImage.normalize();
-            break;
-          default:
-        }
-        const transformedImage = await jimpImage.getBase64Async(Jimp.MIME_JPEG);
-        setTransformedImage(transformedImage);
-      };
-      transformImage();
-    }
-  }, [jimpImage, filter]);
-
-  return (
-    <>
-      <img className="capturedImage" src={transformedImage} />
-    </>
-  );
+  return <></>;
 }
