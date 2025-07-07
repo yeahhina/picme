d7306a2a12a493b0cfe9cf65bb3ca0865dd64cb7
Yeahhina <138409780+yeahhina@users.noreply.github.com>
2025-07-07 22:37:03 +0100

    added fonts

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./components/layout/App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import "./assets/fonts/whiteRabbit.ttf";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
